98b03ecc49b0d16e97739a40c51e20ea2334c2ac
sghanekar-ksmc <sghanekar@resultant.com>
2022-05-25 19:30:24 -0400

    re-added postgres function to template

--- adding_postgres_lib/app.py
+++ adding_postgres_lib/app.py
@@ -2,21 +2,15 @@ import json
 import pandas
 import datetime
 import re
-# import psycopg2
+import psycopg2
 # import requests
 
 
 def validate(event, context):
     
-    email_regex = re.compile('^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
-    matches1 = email_regex.match("a@b@c@--d@resultantcom") != None
-    matches2 = email_regex.match("a@b@c@--d@resultant.com") != None
-    matches3 = email_regex.match("xyz@resultant.com") != None
-    a = np.array([1, 2, 3])
-    print(a)
     response = {
         'statusCode': 200,
-        'body': json.dumps({ 'result1': matches1 , 'result2': matches2, 'result3': matches3})
+        'body': json.dumps({"psycopg2 import successful"})
     }
 
     return response
