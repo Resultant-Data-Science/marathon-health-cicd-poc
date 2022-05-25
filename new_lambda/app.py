4391fe58d66dd7203c37205f61e042c45a45b4ce
sghanekar-ksmc <sghanekar@resultant.com>
2022-05-25 18:04:01 -0400

    Update app.py
    Dummy change to test deployment

--- new_lambda/app.py
+++ new_lambda/app.py
@@ -38,7 +38,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world ",
+            "message": "Hello World!",
             # "location": ip.text.replace("\n", "")
         }),
     }
